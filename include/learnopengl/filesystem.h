84470fcf09fb8ee3328afe469b8160e03fd9bfa1
gapindang <rizqullah.zuhayr.2405336@students.um.ac.id>
2026-03-03 16:23:56 +0700

    minggu 6 exercise done

---
+++ include/learnopengl/filesystem.h
@@ -0,0 +1,15 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+
+#include <string>
+
+class FileSystem
+{
+public:
+    static std::string getPath(const std::string &path)
+    {
+        return path;
+    }
+};
+
+#endif
